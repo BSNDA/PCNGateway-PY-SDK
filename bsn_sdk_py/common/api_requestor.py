422b882d621a244121e2fce99e92a3c34c51538d
helailiang <hllfight@163.com>
2021-11-13 14:35:42 +0800

    add comment

--- bsn_sdk_py/common/api_requestor.py
+++ bsn_sdk_py/common/api_requestor.py
@@ -1,29 +1,29 @@
 import requests
 from bsn_sdk_py.client.exceptions import BsnException
 from bsn_sdk_py.client.bsn_enum import ResCode
-from bsn_sdk_py.until.bsn_logger import log_debug,log_info
-import  urllib3
+from bsn_sdk_py.until.bsn_logger import log_debug, log_info
+import urllib3
 import logging
 
 logging.captureWarnings(True)
 urllib3.disable_warnings()  ## Cancel the HTTPS request 2020/08/25
 
+
 class APIRequestor(object):
     '''
     common request object
     '''
-
     def __init__(self, cert_path=False):
         self.cert_path = cert_path
 
     def request_post(self, req_url, data):
         """
-        :description  :
-        :param  :
-        :return  :
+        :description  : post request
+        :param  : req_url : request url
+        :param  : data : request data
+        :return  : 
         """
-        
-        
+
         log_info(("request address：", req_url))
         log_info(("request data：", data))
         headers = {'content-type': 'application/json'}
@@ -36,8 +36,10 @@ class APIRequestor(object):
             raise Exception('request failure,http code:{}'.format(resCode, ))
         resBody = res.json()
         log_info(('response：', resBody))
-        if resBody['header']["code"] != ResCode.ResCode_Suc.value:  # return success, decrypt 
-            raise BsnException(resBody['header']["code"],resBody['header']["msg"])
+        if resBody['header'][
+                "code"] != ResCode.ResCode_Suc.value:  # return success, decrypt
+            raise BsnException(resBody['header']["code"],
+                               resBody['header']["msg"])
         return resBody
 
     def request_get(self):
